47a9a502bb953e8a128de357cffa98d0c7b3cc01
sungbin5304 <sungbin.dev@gmail.com>
2020-10-14 16:44:16 +0900

    :lemon: theme update :lemon:

--- app/src/main/kotlin/me/sungbin/portfolio/MainActivity.kt
+++ app/src/main/kotlin/me/sungbin/portfolio/MainActivity.kt
@@ -1,11 +1,17 @@
 package me.sungbin.portfolio
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
