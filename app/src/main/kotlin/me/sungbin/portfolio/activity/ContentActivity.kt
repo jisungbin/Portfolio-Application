3e5a593db72c5d91fe2edb86b1783864a8dc4756
sungbin5304 <sungbin.dev@gmail.com>
2020-10-14 20:26:28 +0900

    :watermelon: update fragment :watermelon:

--- app/src/main/kotlin/me/sungbin/portfolio/activity/ContentActivity.kt
+++ app/src/main/kotlin/me/sungbin/portfolio/activity/ContentActivity.kt
@@ -3,6 +3,7 @@ package me.sungbin.portfolio.activity
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_content.*
 import me.sungbin.portfolio.R
@@ -11,6 +12,17 @@ class ContentActivity : AppCompatActivity() {
 
     private val navController by lazy {
         (supportFragmentManager.findFragmentById(R.id.fcv_container) as NavHostFragment).navController
+    }
+
+    private val listener = NavController.OnDestinationChangedListener { controller, _, _ ->
+        with(tv_title) {
+            when (controller.currentDestination?.id) {
+                R.id.introduce -> text = getString(R.string.content_profile)
+                R.id.skill -> text = getString(R.string.content_skill)
+                R.id.android -> text = getString(R.string.content_android)
+                R.id.info -> text = getString(R.string.content_info)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +35,16 @@ class ContentActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_navigation, menu)
         sbb_navigation.setupWithNavController(menu!!, navController)
         return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        navController.removeOnDestinationChangedListener(listener)
+        super.onPause()
     }
 
 }
